161509967a8a0f41c4b41a2e9effbce827a4e43d
sleepyhaha <131776408+sleepyhaha@users.noreply.github.com>
2023-07-30 23:23:49 +1000

    added template shapes and classes for shapes

---
+++ index.js
@@ -0,0 +1,35 @@
+class Rectangle {
+  constructor(x, y, width, height, fill) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.fill = fill;
+  }
+}
+
+class Circle {
+  constructor(r, cx, cy, fill) {
+    this.r = r;
+    this.cx = cx;
+    this.cy = cy;
+    this.fill = fill;
+  }
+}
+
+// polygon = triangle
+class Triangle {
+  constructor(points, fill) {
+    this.points = points;
+    this.fill = fill;
+  }
+}
+
+class LogoText {
+  constructor(fontsize, textanchor, x, y) {
+    this.fontsize = fontsize;
+    this.textanchor = textanchor;
+    this.x = x;
+    this.y = y;
+  }
+}
