3cf4ea9e28399499d0166265a409c7034d662c21
sleepyhaha <131776408+sleepyhaha@users.noreply.github.com>
2023-08-02 21:45:31 +1000

    added validation and colour list

--- index.js
+++ index.js
@@ -1,59 +1,52 @@
-class Rectangle {
-  constructor(x, y, width, height, fill) {
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
-    this.fill = fill;
-  }
-}
-
-class Circle {
-  constructor(r, cx, cy, fill) {
-    this.r = r;
-    this.cx = cx;
-    this.cy = cy;
-    this.fill = fill;
-  }
-}
-
-// polygon = triangle
-class Triangle {
-  constructor(points, fill) {
-    this.points = points;
-    this.fill = fill;
-  }
-}
-
-class LogoText {
-  constructor(fontsize, textanchor, x, y) {
-    this.fontsize = fontsize;
-    this.textanchor = textanchor;
-    this.x = x;
-    this.y = y;
-  }
-}
+const colourList = require("./lib/colour.json");
+const colourArray = Object.entries(colourList);
 
 const questions = [
   {
     type: "input",
     message: "Please enter your logo text (no more than 3 characters plz)",
-    name: "logotext",
+    name: "logoText",
+    validate: (input) => {
+      if (input.length <= 3 && input.length > 0) {
+        return true;
+      } else {
+        console.log("Text must be between 0 - 3 characters.");
+        return false;
+      }
+    },
   },
   {
     type: "input",
     message: "Please enter the text colour",
     name: "textColour",
+    validate: (input) => {
+      let inputLower = input.toLowerCase();
+      if (colourArray.flat().includes(inputLower) === true) {
+        return;
+      } else {
+        console.log("Please enter a different colour");
+        return false;
+      }
+    },
   },
   {
     type: "input",
     message: "Please enter the background colour",
     name: "logoColour",
+    validate: (input) => {
+      let inputLower = input.toLowerCase();
+      if (colourArray.flat().includes(inputLower) === true) {
+        return;
+      } else {
+        console.log("Please enter a different colour");
+        return false;
+      }
+    },
   },
   {
     type: "list",
     message: "Please choose a shape",
     name: "shape",
-    choices: ["circle", "square", "triangle"],
+    choices: ["Circle", "Square", "Triangle"],
   },
 ];
