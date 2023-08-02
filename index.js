76da161d34e1c3007c7d6169a9fb2612ad04914e
sleepyhaha <131776408+sleepyhaha@users.noreply.github.com>
2023-08-02 22:10:52 +1000

    added functions for inquirer and logo generation

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateLogo = require("./lib/logoGen");
 const colourList = require("./lib/colour.json");
+const { error } = require("console");
 const colourArray = Object.entries(colourList);
 
 const questions = [
@@ -22,7 +26,7 @@ const questions = [
     validate: (input) => {
       let inputLower = input.toLowerCase();
       if (colourArray.flat().includes(inputLower) === true) {
-        return;
+        return true;
       } else {
         console.log("Please enter a different colour");
         return false;
@@ -36,7 +40,7 @@ const questions = [
     validate: (input) => {
       let inputLower = input.toLowerCase();
       if (colourArray.flat().includes(inputLower) === true) {
-        return;
+        return true;
       } else {
         console.log("Please enter a different colour");
         return false;
@@ -50,3 +54,24 @@ const questions = [
     choices: ["Circle", "Square", "Triangle"],
   },
 ];
+
+const writeSVG = (fileName, data) => {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+};
+
+const generateSVG = async () => {
+  try {
+    const userInput = await inquirer.prompt(questions);
+    const finalSVG = generateLogo(userInput);
+    writeSVG("./output/logo.svg", finalSVG);
+    console.log("Logo generated. Please find your logo in the output folder.");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+generateSVG();
