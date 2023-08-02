c11e5d60f0551e8f4a4f75d18ee59a549785a9f4
sleepyhaha <131776408+sleepyhaha@users.noreply.github.com>
2023-08-02 23:30:10 +1000

    fixed failling test and added extra test

--- tests/colour.test.js
+++ tests/colour.test.js
@@ -6,6 +6,12 @@ test("Colour found in list", () => {
   expect(colourArray.flat().includes("blue")).toBeTruthy();
 });
 
+test("Hexcode found in list", () => {
+  let input = "#ffEBCD";
+  let inputLower = input.toLowerCase();
+  expect(colourArray.flat().includes(inputLower)).toBeTruthy();
+});
+
 test("Colour not found in list", () => {
   expect(colourArray.flat().includes("UHHHHH")).toBeFalsy();
 });
--- tests/logoGen.test.js
+++ tests/logoGen.test.js
@@ -5,7 +5,7 @@ test("Circle", () => {
   expect(circle.render()).toEqual(
     `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 200" width="300" height="200">
   <circle cx="150" cy="100" r="75" fill="purple" stroke="black" stroke-width="4"/>
-  <text x="50%" y="50%" text-anchor="middle" dominant-baseline="middle" font-size="56" font-weight="bold" fill="green">PLS</text>
+  <text x="50%" y="50%" text-anchor="middle" dominant-baseline="middle" font-size="56" font-weight="bold" font-family="Verdana" fill="green">PLS</text>
 </svg>`
   );
 });
@@ -15,7 +15,7 @@ test("Square", () => {
   expect(square.render()).toEqual(
     `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 200" width="300" height="200">
   <rect x="75" y="25" width="150" height="150" fill="blue" stroke="black" stroke-width="4"/>
-  <text x="50%" y="50%" text-anchor="middle" dominant-baseline="middle" font-size="56" font-weight="bold" fill="black">YES</text>
+  <text x="50%" y="50%" text-anchor="middle" dominant-baseline="middle" font-size="56" font-weight="bold" font-family="Verdana" fill="black">YES</text>
 </svg>`
   );
 });
@@ -25,7 +25,7 @@ test("Triangle", () => {
   expect(triangle.render()).toEqual(
     `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 200" width="300" height="200">
   <polygon points="150,25 275,175 25,175" fill="red" stroke="black" stroke-width="4"/>
-  <text x="150" y="135" text-anchor="middle" dominant-baseline="middle" font-size="56" font-weight="bold" fill="skyblue">WTF</text>
+  <text x="150" y="135" text-anchor="middle" dominant-baseline="middle" font-size="56" font-weight="bold" font-family="Verdana" fill="skyblue">WTF</text>
 </svg>`
   );
 });
